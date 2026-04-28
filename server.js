aef371f19054af27e372fd34c3d3d778962fea9a
Aditya Deshmukh <108890472+A7-arjun@users.noreply.github.com>
2026-04-29 04:18:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    let filePath = "./public" + req.url;
+
+    // Default file
+    if (req.url === "/") {
+        filePath = "./public/index.html";
+    }
+
+    let extname = path.extname(filePath);
+
+    let contentType = "text/html";
+
+    if (extname === ".css") contentType = "text/css";
+    if (extname === ".js") contentType = "text/javascript";
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end("File Not Found");
+        } else {
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(content);
+        }
+    });
+
+});
+
+server.listen(port, () => {
+    console.log("Server running at http://localhost:3000");
+});
